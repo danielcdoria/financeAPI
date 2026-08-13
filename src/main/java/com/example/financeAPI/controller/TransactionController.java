43550b1874c7f188e355a04050f1ca6fa0ad1c0f
Daniel Carvalho Doria <danielcarvalhodoria@gmail.com>
2026-08-13 08:56:22 -0300

    creation of finance project

--- src/main/java/com/example/financeAPI/controller/TransactionController.java
+++ src/main/java/com/example/financeAPI/controller/TransactionController.java
@@ -2,7 +2,10 @@ package com.example.financeAPI.controller;
 
 import com.example.financeAPI.dtos.transactionDtos.TransactionRequestDto;
 import com.example.financeAPI.dtos.transactionDtos.TransactionResponseDto;
+import com.example.financeAPI.models.Transaction;
+import com.example.financeAPI.models.User;
 import com.example.financeAPI.service.TransactionService;
+import jdk.jfr.Category;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,7 +30,26 @@ public class TransactionController {
     }
 
     @GetMapping("/transactions/{id}")
-    public ResponseEntity<TransactionResponseDto> findTransaction(@PathVariable Long id){
-        
+    public ResponseEntity<TransactionResponseDto> findById(@PathVariable Long id){
+        TransactionResponseDto transaction = service.findById(id);
+        return ResponseEntity.ok(transaction);
+    }
+
+    @DeleteMapping("/transactions/{id}")
+    public ResponseEntity<String> remove(@PathVariable Long id){
+        String message = service.remove(id);
+        return ResponseEntity.ok(message);
+    }
+
+    @GetMapping("/transactions/type")
+    public ResponseEntity<List<TransactionResponseDto>> findByType(@RequestParam Transaction.Type type){
+        List<TransactionResponseDto> list = service.findByType(type);
+        return ResponseEntity.ok(list);
+    }
+
+    @GetMapping("/transactions/category")
+    public ResponseEntity<List<TransactionResponseDto>> findByCategory(@RequestParam String category){
+        List<TransactionResponseDto> list = service.findByCategory(category);
+        return ResponseEntity.ok(list);
     }
 }
