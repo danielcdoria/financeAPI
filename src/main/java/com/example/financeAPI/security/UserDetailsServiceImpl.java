703154bbc0eb4fc06d25d39fa9bd8741ac05c728
Daniel Carvalho Doria <danielcarvalhodoria@gmail.com>
2026-08-20 19:16:46 -0300

    fix erro

--- src/main/java/com/example/financeAPI/security/UserDetailsServiceImpl.java
+++ src/main/java/com/example/financeAPI/security/UserDetailsServiceImpl.java
@@ -15,7 +15,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     }
 
     @Override
-    public UserDetails loadUserByUsername(String email)throws UsernameNotFoundException{
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException{
         return userRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found: " + email));
     }
